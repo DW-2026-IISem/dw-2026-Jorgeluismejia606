ee007e66c13578b2068368ad201e251c0ea071ee
Jorge <jorgemejiamejia466@gmail.com>
2026-09-16 10:08:44 -0500

    feat: add models for Categoria, Autor and Sede

---
+++ src/features/business/autores/infrastructure/persistence/models/autor.model.ts
@@ -0,0 +1,18 @@
+import { Table, Column, Model, DataType, PrimaryKey, AutoIncrement } from 'sequelize-typescript';
+
+@Table({ tableName: 'autores' })
+export class AutorModel extends Model {
+  @PrimaryKey
+  @AutoIncrement
+  @Column(DataType.INTEGER)
+  declare id: number;
+
+  @Column({ type: DataType.STRING(150), allowNull: false })
+  declare nombre: string;
+
+  @Column({ type: DataType.STRING(255), allowNull: true })
+  declare descripcion: string;
+
+  @Column({ type: DataType.BOOLEAN, defaultValue: true })
+  declare is_active: boolean;
+}
---
+++ src/features/business/sedes/infrastructure/persistence/models/sede.model.ts
@@ -0,0 +1,18 @@
+import { Table, Column, Model, DataType, PrimaryKey, AutoIncrement } from 'sequelize-typescript';
+
+@Table({ tableName: 'sedes' })
+export class SedeModel extends Model {
+  @PrimaryKey
+  @AutoIncrement
+  @Column(DataType.INTEGER)
+  declare id: number;
+
+  @Column({ type: DataType.STRING(100), allowNull: false })
+  declare nombre: string;
+
+  @Column({ type: DataType.STRING(255), allowNull: true })
+  declare descripcion: string;
+
+  @Column({ type: DataType.BOOLEAN, defaultValue: true })
+  declare is_active: boolean;
+}
