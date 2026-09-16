98f607522e80a2867cdaaa3a37059895fe450990
Jorge <jorgemejiamejia466@gmail.com>
2026-09-16 09:56:18 -0500

    feat: configure swagger openapi documentation

---
+++ src/config/swagger/swagger.config.ts
@@ -0,0 +1,14 @@
+import { INestApplication } from '@nestjs/common';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
+
+export function setupSwagger(app: INestApplication): void {
+  const config = new DocumentBuilder()
+    .setTitle('LecturaAbierta API')
+    .setDescription('Backend REST para la Red de Bibliotecas LecturaAbierta')
+    .setVersion('1.0')
+    .addBearerAuth({ type: 'http', scheme: 'bearer', bearerFormat: 'JWT' }, 'access-token')
+    .build();
+
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api/docs', app, document);
+}
